5673f7fc88480196ea76b32aacbded825f37560c
Hauke Rehfeld <hauke@haukerehfeld.de>
2017-04-11 12:38:39 +0200

    chg: better comment

--- smart-hungry-delete.el
+++ smart-hungry-delete.el
@@ -30,7 +30,18 @@
 
 ;;; Commentary:
 
-;; Delete whitespace between words with some smartness (dumbness) about words, parenthesis and other delimiters
+;; Hungrily deletes whitespace between cursor and next word, paren or
+;; delimiter while honoring some rules about where space should be
+;; left to separate words and parentheses.
+;;
+;; Usage:
+;;
+;; with use-package:
+;; 
+;; (use-package smart-hungry-delete
+;;   :bind (("<backspace>" . smart-hungry-delete/backward-char)
+;; 		 ("C-d" . smart-hungry-delete/forward-char)))
+
 
 ;;; Code:
 
